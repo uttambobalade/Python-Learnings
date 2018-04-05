7bcdde2a849475408c7b6c8a85b86f8964043d73
uttambobalade <uttam.bobalade@gmail.com>
2018-04-04 20:26:20 -0500

    Numeric Data

---
+++ Numeric Data.py
@@ -0,0 +1,28 @@
+num = 3
+fl_num = 2.445
+print(type(num)) # prints type of variable , Int
+print(type(fl_num)) # prints type as a Float
+
+# Arithmatic operations:-
+# Addition 4 + 3
+# Subtraction 4 - 3
+# Multiplication 4 * 3
+# division 4 / 3
+# floor division 4 // 3
+# Exponent 4 ** 3
+# Modulus 4 % 3
+
+print(4 + 3) # prints 7
+print(4 - 3) # prints 1
+print(4 * 3) # prints 12
+print(4 / 3) # prints 1.3333333
+print(4 // 3) # prints 1
+print(4 ** 3) # prints 64
+print(4 % 3) # prints 1
+
+
+# Normal arthmatic operations
+
+print ( 4 + 5 * 3) # prints 19
+
+print ( 5 * 3 + 2 * (5 + 1)) # prints 27

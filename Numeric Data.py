d2d0fd9491e262321283df930c97cfad918ec014
uttambobalade <uttam.bobalade@gmail.com>
2018-04-04 23:01:10 -0500

    Numeric Data.py

--- Numeric Data.py
+++ Numeric Data.py
@@ -22,7 +22,27 @@ print(4 % 3) # prints 1
 
 
 # Normal arthmatic operations
-
 print ( 4 + 5 * 3) # prints 19
-
 print ( 5 * 3 + 2 * (5 + 1)) # prints 27
+
+# Built in functions for Numbers
+print(abs(-12)) # prints 12
+print(round(3.75245)) # prints 4.
+print(round(3.75245,2)) # prints 3.75
+
+# arthmetic operators
+num1 = 3
+num2 = 2
+
+print( num1 == num2) # prints False
+print( num1 > num2) # prints True
+print( num1 < num2) # prints False
+
+strNum1 = '100'
+strNum2 = '200'
+
+print(strNum1 + strNum2) # prints 100200 as like str concatenation
+
+strNum1 = int(strNum1)
+strNum2 = int(strNum2)
+print(strNum1 + strNum2) # prints value 300

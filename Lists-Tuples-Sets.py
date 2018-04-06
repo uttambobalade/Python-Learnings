bac7c8605f2892def9ba1c43a9e6794fc5dfeda3
uttambobalade <uttam.bobalade@gmail.com>
2018-04-05 22:41:37 -0500

    Lists-Tuples-Sets.py

--- Lists-Tuples-Sets.py
+++ Lists-Tuples-Sets.py
@@ -39,3 +39,44 @@ print(courses.index('compsci')) # prints index of input value from list , here o
 # in operator will check value is present or not in list by returning true/false
 print('compsci' in courses) # prints True
 print('biology' in courses) # prints False
+
+# join() method converts list object into string with seperator
+course_str = ' - '.join(courses) # prints list values with '-' seperator
+print(course_str)
+# back to list object from string using split() function
+new_list = course_str.split(' - ') # retuns list object
+print(new_list)
+
+
+### Tuples ####
+
+# tuples are Immutable and lists are Mutable, paranthesis are used in tuples for assigning object
+tuple_1 = ('physics', 'education', 'compsci', 'art')
+print(tuple_1)
+
+### Sets ###
+# sets contain unordered and no duplicate values
+cs_courses = {'physics', 'education', 'compsci', 'it'}
+print(cs_courses) # prints un ordered values from list
+
+art_courses = {'art','it','env sci', 'education','design'}
+
+# intersection() - common from both lists
+print(cs_courses.intersection(art_courses))
+# Difference() - different values from lists 2
+print(cs_courses.difference(art_courses))
+# Union() - All values from list one and non common values from list2
+print(cs_courses.union(art_courses))
+
+
+# Empty Lists
+l1 = []
+l2 = list()
+
+# Empty tuples
+t1 = ()
+t2 = tuple()
+
+# empty sets
+# s1 = {} # this isn't right , it's dictonary
+s2 = set()

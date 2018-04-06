87638b5cf70b73829bf538c8030d07c6f1db5264
uttambobalade <uttam.bobalade@gmail.com>
2018-04-05 22:12:11 -0500

    Lists-Tuples-Sets.py

--- Lists-Tuples-Sets.py
+++ Lists-Tuples-Sets.py
@@ -1,6 +1,6 @@
 # Lists
 courses =['History', 'math', 'physics','compsci']
-print(courses)
+
 print(courses[3]) # prints value of 3 index
 print(courses[-1]) # negative indexesgave last items - here output is 'compsci'
 print(courses[-2]) # prints physics
@@ -9,27 +9,33 @@ print(courses[0:2]) # first param is starting point nad second param is stoping 
 
 # Lists mehtod
 courses.append('art') # Append () adds value at last
-print(courses)
 
 courses.insert(1, 'art') # insert () adds value at given index
-print(courses)
 
 courses_2 =['education','chemistry'] # another list object
-
 # try to insert one list object into another
 #courses.insert(0,courses_2)
 print(courses) # prints [['education', 'chemistry'], 'History', 'art', 'math', 'physics', 'compsci', 'art']
 
 # To overcome on above issue, will use extend() function
-courses.extend(courses_2)
-print(courses) # add all values from second list into first one
+courses.extend(courses_2) # add all values from second list into first one
 
 courses.remove('math') # removes perticular value
-print(courses)
 rmstr = courses.pop() # removes last value from list and pop() returns removed value
-print(courses)
 print(rmstr) # prints removed value
 
 # reverse List
 courses.reverse()
-print(courses)
+# sort list - stirng sorts alphabetical and number sorts ascending order
+courses.sort() # sorts list with alphabetical/ascending order
+
+courses.sort(reverse=True) # sorts list decending order with param
+
+sorted_courses = sorted(courses) # returns sorted copy input list without modifing input lists
+print(sorted_courses)
+
+print(courses.index('compsci')) # prints index of input value from list , here output is 2
+
+# in operator will check value is present or not in list by returning true/false
+print('compsci' in courses) # prints True
+print('biology' in courses) # prints False

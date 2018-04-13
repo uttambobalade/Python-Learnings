afa03ea6e4e596d2074010a0120f2e24ffa8da74
uttambobalade <uttam.bobalade@gmail.com>
2018-04-12 21:36:17 -0500

    Functions.py

--- Functions.py
+++ Functions.py
@@ -12,3 +12,10 @@ def print_message(message):
     return 'Hello {} welcome to this course. '.format(message)
 
 print(print_message("Uttam")) # prints hello uttam welcome to this course
+
+# function with default value param
+def default_param(message, name='You'):
+    return '{}, {}'.format(message, name)
+
+print(default_param('Hi')) # print Hi with default value of param 'name'
+print(default_param('Hi','Uttam')) # prints Hi Uttam
